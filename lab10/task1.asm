c447f7219537cacbe0f719d22cfab64b85c0bbbc
Substance <shpion160806@gmail.com>
2025-05-22 13:54:49 +0300

    added lab10

--- lab10/task1.asm
+++ lab10/task1.asm
@@ -6,25 +6,46 @@ includelib c:\masm32\lib\kernel32.lib
 ExitProcess proto :DWORD
 
 .data
-x DB 5
-y DB 5
+x DB 2
+y DB 2
 z DW ?
 
 .code
 
 main:
-    
+    MOV BL, x
+    MOV BH, y
+    CALL pr
+    CMP AX, 5
+    JL second
+
+    SUB BL, 2
+    INC BH
+    CALL pr
+    JMP fin
+
+    second:
+    ADD AX, 3
+
+    fin:
+    MOV z, AX
+
     push 0
     call ExitProcess
 
 ; AX = 2 * BL + 3 * BH
 pr proc
+    PUSH DX
+
     MOV AL, 2
     IMUL BL
     MOV DX, AX
     MOV AL, 3
     IMUL BH
     ADD AX, DX
+
+    POP DX
+    RET
 pr endp
 
 end main
