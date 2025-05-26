6e8781d7e29a0fbde16feb266d9eae931ebea6fe
Substance <shpion160806@gmail.com>
2025-05-26 10:24:48 +0300

    added lab11

---
+++ lab11/task1.asm
@@ -0,0 +1,56 @@
+.586
+.model flat, stdcall
+.stack 4096
+
+includelib c:\masm32\lib\kernel32.lib
+ExitProcess proto :DWORD
+
+.data
+n1 equ 7
+n2 equ 5
+a1 DB 1,0,2,4,5,7,-1; -> -2,0,2,4,2,4,-4
+a2 DB 0,0,11,10,99; -> 0,0,8,10,96
+
+.code
+;уменьшить на 3 каждый нечетный элемент
+main:
+    LEA EAX, a1
+    MOV ECX, n1
+    CALL pr
+
+    LEA EAX, a2
+    MOV ECX, n2
+    CALL pr
+
+    push 0
+    call ExitProcess
+
+; AX = 2 * BL + 3 * BH
+pr proc
+
+    PUSH EDX
+    PUSH EBX
+    PUSH ECX
+
+    MOV EBX, 0
+
+    prL1:
+    TEST BYTE PTR [EAX+EBX], 1b
+    JZ next
+
+    MOV DL, [EAX+EBX]
+    SUB DL, 3
+    MOV [EAX+EBX], DL
+
+    next:
+    INC EBX
+    LOOP prL1
+
+    POP ECX
+    POP EBX
+    POP EDX
+
+    RET
+pr endp
+
+end main
